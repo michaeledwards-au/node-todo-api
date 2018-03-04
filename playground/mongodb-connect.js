2d4fdd1a41aaed3ebde0bdf534332726025b96d9
Michael Edwards <me@sh32my.com>
2018-03-04 13:04:10 +1100

    test commit with key

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -11,6 +11,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
 
+  //
   // db.collection('Todos').insertOne({
   //   text: 'Something to do',
   //   completed: false
