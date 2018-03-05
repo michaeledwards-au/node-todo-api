4b44f526387f4550c02dfddd42d9e1d40d494d49
Michael Edwards <me@sh32my.com>
2018-03-06 09:58:05 +1100

    Add tests for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -93,3 +93,44 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(todos[1]._id).then((todo) => {
+          expect(todo).toBe(null);
+          done();
+        }, (e) => {
+          done(e);
+        });
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectId().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .delete('/todos/123abc')
+      .expect(404)
+      .end(done)
+  });
+});
