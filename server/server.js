ffef869182da2f5ecf70103a305dd108439feeb1
Michael Edwards <me@sh32my.com>
2018-03-07 15:09:37 +1100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -97,6 +98,18 @@ app.patch('/todos/:id', (req, res) => {
   });
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -109,7 +122,6 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   });
 });
-
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
